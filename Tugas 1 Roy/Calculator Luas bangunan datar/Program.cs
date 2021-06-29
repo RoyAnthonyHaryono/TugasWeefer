de3c38f8e15985dc3180e7c30cb594460e3dd44f
Roy Anthony Haryono <86603734+RoyAnthonyHaryono@users.noreply.github.com>
2021-06-29 20:36:48 +0700

    Add files via upload

---
+++ Tugas 1 Roy/Calculator Luas bangunan datar/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator_Luas_bangunan_datar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Selamat Datang di aplikasi Calculator luas bangunan datar");
+                bool forloop = true;
+                while (forloop == true)
+              {
+                Console.WriteLine("Luas bangunan apa yang kalian mau hitung? (segitiga, persegi, trapesium, lingkaran)");
+                Console.WriteLine("Tekan angka 1 untuk segitiga");
+                Console.WriteLine("Tekan angka 2 untuk persegi");
+                Console.WriteLine("Tekan angka 3 untuk trapesium");
+                Console.WriteLine("Tekan angka 4 untuk lingkaran");
+                Console.WriteLine("Tekan angka 5 untuk keluar");
+                int bangunandatar;
+                bool valid = (int.TryParse(Console.ReadLine(), out bangunandatar));
+                while (!valid)
+                {
+                    Console.WriteLine("tolong hanya isikan angka");
+                    valid = int.TryParse(Console.ReadLine(), out bangunandatar);
+                }
+                if (bangunandatar == 1)
+                {
+                    Console.WriteLine("Tolong gunakan tanda koma untuk angka desimal!");
+                    Console.WriteLine("Tolong masukkan ukuran alas segitiga");                  
+                    double alas;
+                    bool berhasil = (double.TryParse(Console.ReadLine(), out alas));
+                    while (!berhasil)
+                    {
+                        Console.WriteLine("Tolong hanya masukkan angka saja");
+                        berhasil = (double.TryParse(Console.ReadLine(), out alas));
+                    }
+                    Console.WriteLine("Tolong masukkan ukuran tinggi segitiga");                   
+                    double tinggi;
+                    bool berhasil1 = (double.TryParse(Console.ReadLine(), out tinggi));
+                    while (!berhasil1)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil1 = (double.TryParse(Console.ReadLine(), out tinggi));
+                    }    
+                    double hasilsegitiga = alas * tinggi / 2;
+                    Console.WriteLine("Hasil= " + hasilsegitiga + " satuan");
+                    Console.WriteLine("Terima kasih sudah menggunakan aplikasi kami");
+                }
+                if (bangunandatar == 2)
+                {
+                    Console.WriteLine("Tolong gunakan tanda koma untuk angka desimal!");
+                    Console.WriteLine("Tolong masukkan ukuran panjang persegi");                   
+                    double panjang;
+                    bool berhasil = (double.TryParse(Console.ReadLine(), out panjang));
+                    while (!berhasil)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil = (double.TryParse(Console.ReadLine(), out panjang));
+                    }
+                    Console.WriteLine("Tolong masukkan ukuran lebar persegi");                   
+                    double lebar;
+                    bool berhasil1 = (double.TryParse(Console.ReadLine(), out lebar));
+                    while (!berhasil1)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil1 = (double.TryParse(Console.ReadLine(), out lebar));
+                    }                   
+                    double hasilpersegi = panjang * lebar;
+                    Console.WriteLine("Hasil= " + hasilpersegi + " satuan");
+                    Console.WriteLine("Terima kasih sudah menggunakan aplikasi kami");
+                }
+                if (bangunandatar == 3)
+                {
+                    Console.WriteLine("Tolong gunakan tanda koma untuk angka desimal!");
+                    Console.WriteLine("Tolong masukkan ukuran alas A trapesium");
+                    double alasA;
+                    bool berhasil = (double.TryParse(Console.ReadLine(), out alasA));
+                    while (!berhasil)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil = (double.TryParse(Console.ReadLine(), out alasA));
+                    }
+                    Console.WriteLine("Tolong masukkan ukuran alas B trapesium");                   
+                    double alasB;
+                    bool berhasil1 = (double.TryParse(Console.ReadLine(), out alasB));
+                    while (!berhasil1)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil1 = (double.TryParse(Console.ReadLine(), out alasB));
+                    }                   
+                    Console.WriteLine("Tolong masukkan ukuran tinggi trapesium");
+                    double tinggi;
+                    bool berhasil2 = (double.TryParse(Console.ReadLine(), out tinggi));
+                    while (!berhasil2)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil2 = (double.TryParse(Console.ReadLine(), out tinggi));
+                    }
+                    double alastotal = alasA + alasB;
+                    double hasiltrapesium = alastotal * tinggi / 2;
+                    Console.WriteLine("Hasil= " + hasiltrapesium + " satuan");
+                    Console.WriteLine("Terima kasih sudah menggunakan aplikasi kami");
+                }
+                if (bangunandatar == 4)
+                {
+                    Console.WriteLine("Tolong gunakan tanda koma untuk angka desimal!");
+                    Console.WriteLine("Tolong masukkan ukuran jari-jari lingkaran");
+                    double jari2;
+                    bool berhasil = (double.TryParse(Console.ReadLine(), out jari2));
+                    while (!berhasil)
+                    {
+                        Console.WriteLine("Tolong Hanya masukkan angka saja");
+                        berhasil = (double.TryParse(Console.ReadLine(), out jari2));
+                    }
+                    double pi = 3.14;
+                    double hasillingkaran = pi * jari2 * jari2;
+                    Console.WriteLine("Hasil= " + hasillingkaran + " satuan");
+                    Console.WriteLine("Terima kasih sudah menggunakan aplikasi kami");
+                }
+          
+                if (bangunandatar == 5)
+                {
+                    forloop = false;
+                    Console.WriteLine("Terima kasih sudah menggunakan aplikasi kami");
+                }
+                else
+                {
+                    Console.WriteLine("Tolong masukkan angka dari 1 sampai 5 saja");
+                    forloop = true;
+                }
+                
+               }
+                
+               }
+
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+    }
+
---
+++ Tugas 2 Roy/Tugas 2 Roy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tugas_2_Roy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testing = "Halo! Apa kabar dunia ini? aku akan menjadi raja bajak laut ";
+            List<string> kalimat = new List<string>();
+            int a = 0;
+            int kata1;
+            do
+            {
+                kata1 = testing.IndexOf(" ", a);
+                if (kata1 >= 0)
+                {
+                    kalimat.Add(testing.Substring(a, kata1 - a + 1));
+                    a = kata1 + 1;
+                }
+            } 
+            while (kata1 > 0);
+            foreach (var kalimatcut in kalimat)
+                Console.WriteLine(kalimatcut);
+           
+
+
+
+            
+            
+
+
+
+
+
+
+
+           
+        }
+    }
+}
+
+
